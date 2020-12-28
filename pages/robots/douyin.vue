1cd080ac8b8f39a8882b5f42e5acf7e62f3ea427
yooge <56222194@qq.com>
2020-12-28 23:34:32 +0800

    Revert "入口文件找不到bug"
    This reverts commit e08929d7b9606e268ead08d293a6e703000a5408.

--- pages/robots/douyin.vue
+++ pages/robots/douyin.vue
@@ -83,8 +83,6 @@ export default {
 				console.log('imei: '+device.getIMEI()); //autojs里面的函数
 				//auto();
 				//launchApp('抖音');
-				var x = back2activityEx("停止 | *启动");
-				console.log(x);
 				
 			}); 
 		},
--- static/robots/_entry.js
+++ static/robots/_entry.js
@@ -6,13 +6,12 @@ var path = './' + fname + '.js';
 console.log('>>>>');
 
 if (fname == '_blank') {
-	console.log('_blank');
+	console.log('_blank'); 
 	setTimeout(() => {
 		console.log('_blank done!');
 	}, 60 * 60 * 1000);
 
-} else if (app.args._debug && !files.exists(path)) {
-	//调试环境中，找不到文件
+} else if (!files.exists(path)) {
 	console.log('文件找不到: ' + path);
 } else if (fname) {
 	require('./' + fname + '.js');
